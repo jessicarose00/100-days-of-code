ef2ab3e0b3cb44e0681d99e75b90fe5872f1a5f8
Jessica Rose Ertel <jessertel@mac.com>
2019-11-10 20:23:49 -0500

    add lowercase letters and add a function that updates list of words and points

--- Exercises/Scrabble.py
+++ Exercises/Scrabble.py
@@ -6,6 +6,11 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+# Update the letters list to contain lowercase letters
+letters += [letter.lower() for letter in letters]
+
+points *= 2
+
 # Using list comprehension, create a dictionary that has key value pairs of letters and points.
 # Make sure to add the infamous blank scrabble tile.
 letter_to_points = {key:value for key, value in zip(letters, points)}
@@ -38,14 +43,23 @@ player_to_words = {
 # Create an empty dictionary to track the points for each player.
 player_to_points = {}
 
-# In the player's words dictionary, identify players and words
-# Create another loop that calculates the value of each player's words and adds it to the points variable.
-for player, words in player_to_words.items():
-  player_points = 0
-  for word in words:
-    player_points += score_word(word)
-    player_to_points[player] = player_points
+# Iterate through the player_to_words dictionary and assign each player to player and each list of words to words. 
+# Create another loop that calculates the value of each player's words and adds it to a points variable.
+def update_point_totals():
+  for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+      player_points += score_word(word)
+      player_to_points[player] = player_points
 
-print(player_to_points)
+update_point_totals() 
+      
+# Create a function that takes in a player and word and adds it to the player_to_words dictionary
+def play_word(player, word):
+  player_to_words[player].append(word)
+
+print(player_to_words)
+
+# Turn nested loops into a function that can be called any time a new word is played - addition line 43
 
 
